3ca2902685a29794c67c4f011fddb92127a26a7c
redmonkez12 <tomas.svojanovsky11@gmail.com>
2022-11-28 20:24:16 +0100

    Muj prvni komit

---
+++ main.py
@@ -0,0 +1,10 @@
+from first_folder import hello
+
+hello()
+
+name = "Tomas Svojanovsky"
+print("Hello world")
+print(name)  # alt + insert nebo cmd + n
+# ctrl + c nebo cmd + c ukonci radek a vytvori novou session
+# clear + enter vymaze vsech text a da puvodniho stavu
+g
